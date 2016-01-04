c4451b2dac0315f384fad74e5765981d9377b01b
Ben Rondeau <benrondeau@us.ibm.com>
2016-01-04 12:15:26 -0600

    Added bootstrap, homepage and angular routing

--- TODO.md
+++ TODO.md
@@ -1,9 +1,6 @@
 ### TODO
 
-✓ Create logo for repo README and app homepage
-✓ Setup gulpfile
-- Setup node file to launch up Mongo along with Node server in dev and prod (cloud setup)
-- HTML homepage (header with nav links, dummy content)
-- Setup Gulpfile
+Moved to Asana Project...
+
 
 LOCAL DB Command: mongod --dbpath /Users/benrondeau/Sites/MEAN-Boilerplate/data/db
--- public/js/app.js
+++ public/js/app.js
@@ -1,38 +1,60 @@
-//Angular App hello
-var app = angular.module('MEANapp', []);
+var app = angular.module('MEANapp', ['ngRoute']);
 
-app.controller('MEANcntrl', function($scope, $http){
+app.controller('HomeCntrl', function($scope, $http){
+
    
-   $scope.addName = function(firstName){
-       $scope.postData(firstName);
-       $scope.newName = ''; //resets input
-   };
-   
-   $scope.getData = function(){
-      //Get data from Node Server
-      $http.get('...')
-         .success(function(data){
-            $scope.items = data;
-            console.log('Success calling mongodb');
-         })
-         .error(function(data){
-            alert('Error retrieving data. See console.');
-            console.log(data);
-         });
-   };
-   
-   $scope.postData = function(firstName){
-      $http.post('...', {name: firstName})
-         .success(function(data){
-            $scope.getData(); //refresh page with updated data from DB
-            console.log('Success posting to mongodb');
-         })
-         .error(function(data){
-            alert('Error posting data. See console.');
-            console.log(data);
-         });
-   };
+   //$scope.addName = function(firstName){
+   //    $scope.postData(firstName);
+   //    $scope.newName = ''; //resets input
+   //};
+   //
+   //$scope.getData = function(){
+   //   //Get data from Node Server
+   //   $http.get('...')
+   //      .success(function(data){
+   //         $scope.items = data;
+   //         console.log('Success calling mongodb');
+   //      })
+   //      .error(function(data){
+   //         alert('Error retrieving data. See console.');
+   //         console.log(data);
+   //      });
+   //};
+   //
+   //$scope.postData = function(firstName){
+   //   $http.post('...', {name: firstName})
+   //      .success(function(data){
+   //         $scope.getData(); //refresh page with updated data from DB
+   //         console.log('Success posting to mongodb');
+   //      })
+   //      .error(function(data){
+   //         alert('Error posting data. See console.');
+   //         console.log(data);
+   //      });
+   //};
    
    //$scope.getData();
    
+});
+
+/*********************************
+ Routing
+ *********************************/
+app.config(function($routeProvider) {
+    'use strict';
+
+    $routeProvider.
+
+        //Root
+        when('/', {
+            templateUrl: 'views/home.html',
+            controller: 'HomeCntrl'
+        }).
+
+        //Login page
+        when('/login', {
+            templateUrl: 'views/login.html',
+            controller: 'LoginCntrl'
+        })
+
 });
